7ba5fda324387eb2dff5127bd7dbd3507ae2b3c7
Sebastian Willenborg <seb.willenborg@googlemail.com>
2013-07-16 22:00:45 +0200

    Added missing exception handling in is_open() which caused s0opener to stop

--- eventdistr.py
+++ eventdistr.py
@@ -20,20 +20,19 @@ AREA_BATH = "0"
 AREA_KITCHEN = "K"
 
 
-
-def send_event(name, data=None,version=1):
+def send_event(name, data=None, version=1):
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 	if data:
 		data = "%s=%s" % (name, data)
 	else:
 		data = name
-	s.sendto(EVENTDISTR_COMMAND % (version, data) , (EVENTDISTR_IP,
-		EVENTDISTR_PORT))
+	s.sendto(EVENTDISTR_COMMAND % (version, data),
+			(EVENTDISTR_IP, EVENTDISTR_PORT))
 	s.close()
 
-def parse_event(data):
 
+def parse_event(data):
 	if not data:
 		return None
 	match = EVENTDISTR_RE.match(data)
@@ -44,20 +43,25 @@ def parse_event(data):
 def send_clock(cmd):
 	send_event(cmd, "RISING")
 
+
 def open_door():
 	send_clock(EVENT_DOOR_UNTEN)
+
 
 def ring():
 	send_clock(EVENT_DING_DONG)
 
+
 def now_playing(artist, title, area=AREA_LOUNGE):
 	send_event(EVENT_NOW_PLAYING, "%s\x00%s\x00%s" % (area, artist, title))
 
+
 def virtual_msg(msg):
-	send_event(EVENT_VIRTUAL_MSG, msg) 
+	send_event(EVENT_VIRTUAL_MSG, msg)
+
 
 def run_listener(callback):
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	sock.bind((EVENTDISTR_IP, EVENTDISTR_PORT))
 	while True:
 		data, addr = sock.recvfrom(1024)
